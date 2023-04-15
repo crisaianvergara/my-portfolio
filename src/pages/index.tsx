7c7cd706d2d40fa97e7d352f28ea345e5af470b6
Cris-Aian <crisaianvergara@gmail.com>
2023-04-16 02:01:23 +0800

    updated styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,30 @@
 import styles from '@/styles/Home.module.scss'
 
+import Head from 'next/head';
+
 import Typewriter from 'typewriter-effect';
 
 export default function Home() {
     return (
-        <div className={styles.home}>
-            <h1>Hello, I am Cris-aian.</h1>
-            <h3>
-                <Typewriter
-                    options={{
-                        strings: ['Software Engineer!', 'Day Trader!', 'Farmer!', 'Woodworker!'],
-                        autoStart: true,
-                        loop: true,
-                    }}
-                />
-            </h3>
-        </div>
+        <>
+            <Head>
+                <meta content="Cris-aian Vergara Portfolio Website" name="description"></meta>
+                <meta content="Cris-aian Vergara" name="author"></meta>
+                <meta content="portfolio website" name="keywords"></meta>
+                <title>Cris-aian Vergara | Home</title>
+            </Head>
+            <div className={styles.home}>
+                <h1>Hello, I am Cris-aian.</h1>
+                <h3>
+                    <Typewriter
+                        options={{
+                            strings: ['Software Engineer!', 'Day Trader!', 'Farmer!'],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                </h3>
+            </div>
+        </>
     )
 }
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,8 +1,9 @@
 import styles from '@/styles/Projects.module.scss';
 
 import Link from 'next/link';
+import Head from 'next/head';
 
-import { GithubOutlined, EyeOutlined, DeleteOutlined } from '@ant-design/icons';
+import { GithubOutlined, EyeOutlined, DeleteOutlined, CloseCircleOutlined } from '@ant-design/icons';
 import { Button, Card, Typography } from 'antd';
 
 const { Title } = Typography;
@@ -11,75 +12,78 @@ const { Meta } = Card;
 
 const Projects = () => {
     return (
-        <div className={styles.projects}>
-            <Card className={styles.card}
-                style={{ width: 350 }}
-                cover={
-                    <img
-                        alt="example"
-                        src="/images/zia.png"
+        <>
+            <Head>
+                <meta content="Cris-aian Vergara Portfolio Website" name="description"></meta>
+                <meta content="Cris-aian Vergara" name="author"></meta>
+                <meta content="portfolio website" name="keywords"></meta>
+                <title>Cris-aian Vergara | Projects</title>
+            </Head>
+            <div className={styles.projects}>
+                <Card className={styles.card}
+                    style={{ width: 350 }}
+                    cover={
+                        <img
+                            alt="example"
+                            src="/images/zia.png"
+                        />
+                    }
+                    actions={[
+                        <><Link href="https://www.zia-apps.com/" target='_blank'><EyeOutlined key="zia-url" /></Link></>,
+                        <><Link href="#"><CloseCircleOutlined key="zia-github" /></Link></>,
+                        // <><Button type="link"><DeleteOutlined key="setting" /></Button></>,
+                    ]}
+                    bodyStyle={{ fontSize: '1rem' }}
+                >
+                    <Meta
+                        title={<Title level={4}>Zia-App</Title>}
+                        description="This web app is designed for financial purposes and has been developed using the Django, Next.js, and Ant Design."
                     />
-                }
-                actions={[
-                    <><Link href="https://www.zia-apps.com/" target='_blank'><EyeOutlined key="zia-url" /></Link></>,
-                    <><Link href="/" target='_blank'><GithubOutlined key="zia-github" /></Link></>,
-                    // <><Button type="link"><DeleteOutlined key="setting" /></Button></>,
-                ]}
-                hoverable
-                bodyStyle={{ fontSize: '1rem' }}
-            >
-                <Meta
-                    title={<Title level={4}>Zia-App</Title>}
-                    description="This is a financial website built using the Django and NextJs framework."
-                />
-            </Card>
-            <Card className={styles.card}
-                style={{ width: 350 }}
-                cover={
-                    <img
-                        alt="example"
-                        src="/images/kriskap.png"
+                </Card>
+                <Card className={styles.card}
+                    style={{ width: 350 }}
+                    cover={
+                        <img
+                            alt="example"
+                            src="/images/kriskap.png"
+                        />
+                    }
+                    actions={[
+                        <><Link href="https://kriskap.onrender.com/" target='_blank'><EyeOutlined key="kriskap-url" /></Link></>,
+                        <><Link href="https://github.com/crisaianvergara/kriskap" target='_blank' ><GithubOutlined key="kriskap-github" /></Link></>,
+                        // <><Button type="link"><DeleteOutlined key="setting" /></Button></>,
+                    ]}
+                    bodyStyle={{ fontSize: '1rem' }}
+                >
+                    <Meta
+                        title={<Title level={4}>Kriskap</Title>}
+                        description="This project showcases my technical abilities and demonstrates my understanding of full-stack development."
                     />
-                }
-                actions={[
-                    <><Link href="https://kriskap.onrender.com/" target='_blank'><EyeOutlined key="kriskap-url" /></Link></>,
-                    <><Link href="https://github.com/crisaianvergara/kriskap" target='_blank' ><GithubOutlined key="kriskap-github" /></Link></>,
-                    // <><Button type="link"><DeleteOutlined key="setting" /></Button></>,
-                ]}
-                hoverable
-                bodyStyle={{ fontSize: '1rem' }}
-            >
-                <Meta
-                    title={<Title level={4}>Kriskap</Title>}
-                    description="This is an e-commerce website built using the Flask framework in Python."
-                />
-            </Card>
-            <Card
-                className={styles.card}
-                style={{ width: 350 }}
-                cover={
-                    <img
-                        alt="example"
-                        src="/images/sulat.png"
+                </Card>
+                <Card
+                    className={styles.card}
+                    style={{ width: 350 }}
+                    cover={
+                        <img
+                            alt="example"
+                            src="/images/sulat.png"
+                        />
+                    }
+                    actions={[
+                        <><Link href="https://sulat-aian.onrender.com/" target='_blank' ><EyeOutlined key="sulat-url" /></Link></>,
+                        <><Link href="https://github.com/crisaianvergara/sulat-aian" target='_blank' ><GithubOutlined key="sulat-github" /></Link></>,
+                        // <><Button type="link"><DeleteOutlined key="setting" /></Button></>,
+                    ]}
+                    bodyStyle={{ fontSize: '1rem' }}
+                >
+                    <Meta
+                        title={<Title level={4}>Sulat-Aian</Title>}
+                        description="This is a tech blog web application built using the Flask framework in Python and CSS Bootstrap 5."
                     />
-                }
-                actions={[
-                    <><Link href="/" target='_blank' ><EyeOutlined key="sulat-url" /></Link></>,
-                    <><Link href="https://github.com/crisaianvergara/sulat-aian" target='_blank' ><GithubOutlined key="sulat-github" /></Link></>,
-                    // <><Button type="link"><DeleteOutlined key="setting" /></Button></>,
-                ]}
-                hoverable
-                bodyStyle={{ fontSize: '1rem' }}
-            >
-                <Meta
-                    title={<Title level={4}>Sulat-Aian</Title>}
-                    description="This is a tech blog website built using the Flask framework in Python."
-                />
-            </Card>
-        </div>
-
+                </Card>
+            </div>
+        </>
     )
-
 }
 
 export default Projects
